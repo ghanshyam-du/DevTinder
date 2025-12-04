5801bd71f80b4e0df048e22039ef6b643b730aec
ghanshyam-du <ghanshyamd816@gmail.com>
2025-12-04 23:25:08 +0530

    api is updated

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
-import  {connectDB}  from "./src/config/connection.js";
+import { connectDB } from "./src/config/connection.js";
 import User from "./src/models/user.model.js"
 
 dotenv.config();
@@ -12,34 +12,55 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/signup",async (req, res)=>{
+app.post("/signup", async (req, res) => {
 
     const user = User(req.body);
 
-    try{
-      await user.save();
-      res.send("User added successfully!");
-    }catch(err){
-        res.status(400).send("Error saving the user:"+ err.message);
+    try {
+        await user.save();
+        res.send("User added successfully!");
+    } catch (err) {
+        res.status(400).send("Error saving the user:" + err.message);
     }
 
 })
 
-app.delete("/user", async (req, res)=>{
+app.delete("/user", async (req, res) => {
 
     const userId = req.body.userId;
 
     try {
         const user = await User.findByIdAndDelete(userId);
-       
+
         res.send("User deleted successfully!");
-        
+
     } catch (err) {
-        res.status(400).send("Error while deleting the user!"+ err.message);
-        
+        res.status(400).send("Error while deleting the user!" + err.message);
+
     }
 
 })
+
+// This is the api to find the user by email id ----------------------------------------------we have to update it (pending!)
+// app.get("/user", async (req, res) => {
+//     const emailId = req.query.emailId;
+
+//     if (!emailId) {
+//         return res.status(400).json({ message: "emailId is required" });
+//     }
+
+//     try {
+//         const user = await User.findOne({ emailId });
+
+//         if (!user) {
+//             return res.status(404).json({ message: "User not found!" });
+//         }
+
+//         res.json(user);
+//     } catch (error) {
+//         res.status(500).json({ message: "Server error", error: error.message });
+//     }
+// });
 
 // This api will send you all the data from the databases
 app.get("/feed", async (req, res) => {
